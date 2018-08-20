389b89ef5d54403b9dcb1e562b79ffdd59e3904b
cfire54 <c.stewart5545@gmail.com>
2018-08-20 13:21:14 -0500

    heroku server connect

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-cylindrical-77330',
       user : '',
       password : '',
       database : 'smart-brain'
